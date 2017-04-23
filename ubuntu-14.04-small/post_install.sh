0c32e5c751c18beb1dfb57c89369ccbcf0f5416e
jeremy Hajek <hajek@iit.edu>
2017-04-23 18:05:32 -0500

    Forgot -y on collectd

--- ubuntu-14.04-small/post_install.sh
+++ ubuntu-14.04-small/post_install.sh
@@ -5,7 +5,7 @@ echo password | sudo -S chmod +x /etc/rc.local
 # install latest version of rsyslog
 echo password | sudo -S add-apt-repository -y ppa:adiscon/v8-stable
 # install Collectd for instances
-echo password | sudo -S add-apt-repository ppa:collectd/collectd-5.5
+echo password | sudo -S add-apt-repository -y ppa:collectd/collectd-5.5
 
 echo password | sudo -S apt-get update
 echo password | sudo -S apt-get install -y rsyslog collectd 
@@ -17,16 +17,16 @@ sudo cp -r /tmp/code/collectd/hosta/collectd.d /etc/
 #copy the provided collectd.conf file overwritting the default collectd.conf
 sudo cp /tmp/code/collectd/hosta/collectd.conf /etc/collectd/
 
-sudo update-rc.d collectd defaults
-sudo service collectd start
 
 # Config rsyslog
 # Again assuming that the IP here is the private cloud IP of the Central Rsyslog server
 sudo sed -i "$ a *.* @192.168.250.250:514" /etc/rsyslog.conf
 
 #Modify write Riemann plugin
-sudo sed -i '/Host \"riemanna.\example\.com\"/c\Host \"64.131.111.117\"' /etc/collectd.d/write_riemman.conf
+sudo sed -i '/Host \"riemanna.\example\.com\"/c\Host \"64.131.111.117\"' /etc/collectd.d/write_riemann.conf
 
+sudo update-rc.d collectd defaults
+sudo service collectd start
 
 # Finish modifying for the cloud by stripping the username password via -l
 echo "passwd -l vagrant" > /tmp/shutdown
