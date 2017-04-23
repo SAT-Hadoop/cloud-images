9bb8f8b6632ed890a25c09f0826060c5f68afc1c
jeremy Hajek <hajek@iit.edu>
2017-04-22 19:07:02 -0500

    Fixed spacing

--- ubuntu-14.04-large/post_install.sh
+++ ubuntu-14.04-large/post_install.sh
@@ -21,14 +21,12 @@ sudo cp -r /tmp/code/itmo453-553/collectd/hosta/collectd.d /etc/
 #copy the provided collectd.conf file overwritting the default collectd.conf
 sudo cp /tmp/code/itmo453-553/collectd/hosta/collectd.conf /etc/collectd/
 
-
 sudo update-rc.d collectd defaults
 sudo service collectd start
 
 # Config rsyslog
 # Again assuming that the IP here is the private cloud IP of the Central Rsyslog server
 sudo sed -i "$ a *.* @192.168.250.250:514" /etc/rsyslog.conf
-
 
 # Finish modifying for the cloud by stripping the username password via -l
 echo "passwd -l vagrant" > /tmp/shutdown
