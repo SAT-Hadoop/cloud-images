8d4bc8973667251bce825ad9f0549482ceec43b3
jeremy Hajek <hajek@iit.edu>
2017-04-22 19:02:10 -0500

    Added collectd plugin installs

--- ubuntu-14.04-large/post_install.sh
+++ ubuntu-14.04-large/post_install.sh
@@ -2,37 +2,33 @@
 echo password | sudo -S curl -o /etc/rc.local https://raw.githubusercontent.com/viglesiasce/cloud-images/master/utils/rc.local
 echo password | sudo -S chmod +x /etc/rc.local
 
-
+# install latest version of rsyslog
 echo password | sudo -S add-apt-repository -y ppa:adiscon/v8-stable
-
 
 echo password | sudo -S  apt-get update
 sudo apt-get install -y rsyslog
 
-sudo apt-get install -y ganglia-monitor
+# install Collectd for instances
+sudo sudo add-apt-repository ppa:collectd/collectd-5.5
 
+sudo apt-get update
+sudo apt-get -y install collectd stress
 
+#copy the provided collectd.conf file overwritting the default collectd.conf
+sudo cp ./collectd.conf /etc/collectd/
 
-# the host value is the private IP of the central ganlia server IP
-sudo sed -i '/mcast_join = 239.2.11.71/i \ host = 192.168.19.87' /etc/ganglia/gmond.conf
-sudo sed -i 's/name = "unspecified"/#name = "euca cluster"/g' /etc/ganglia/gmond.conf
-sudo sed -i 's/mcast_join = 239.2.11.71/ #mcast_join = 239.2.11.71/g' /etc/ganglia/gmond.conf
-sudo sed -i 's/bind = 239.2.11.71/#bind = 239.2.11.71/g' /etc/ganglia/gmond.conf
+#Copying the provided and configured collectd.d default plugins to be loaded
+sudo cp -r ./collectd.d /etc/
 
-sudo sed -i 's/port = 8649/#port = 8649/g' /etc/ganglia/gmond.conf
+sudo update-rc.d collectd defaults
+sudo service collectd start
 
-sudo sed -i 's/bind = 239.2.11.71/#bind = 239.2.11.71/g' /etc/ganglia/gmond.conf
-
-sudo service ganglia-monitor restart
-
-
-
-# Install rsyslog
+# Config rsyslog
 # Again assuming that the IP here is the private cloud IP of the Central Rsyslog server
-sudo sed -i "$ a *.* @192.168.19.87:514" /etc/rsyslog.conf
+sudo sed -i "$ a *.* @192.168.250.250:514" /etc/rsyslog.conf
 
 
-
+# Finish modifying for the cloud by stripping the username password via -l
 echo "passwd -l vagrant" > /tmp/shutdown
 echo "rm /tmp/shutdown" >> /tmp/shutdown
 echo "shutdown -P now" >> /tmp/shutdown
