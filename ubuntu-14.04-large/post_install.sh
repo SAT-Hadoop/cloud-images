4283e383a0c7055e806b2e5b9b0377bef7d4200b
jeremy Hajek <hajek@iit.edu>
2017-04-22 19:11:31 -0500

    Added the value for write_riemann

--- ubuntu-14.04-large/post_install.sh
+++ ubuntu-14.04-large/post_install.sh
@@ -28,6 +28,22 @@ sudo service collectd start
 # Again assuming that the IP here is the private cloud IP of the Central Rsyslog server
 sudo sed -i "$ a *.* @192.168.250.250:514" /etc/rsyslog.conf
 
+#Modify write Riemann plugin
+sudo cat << EOT >> /etc/collectd.d/write_riemman.conf
+LoadPlugin write_riemann
+<Plugin "write_riemann">
+    <Node "riemanna">
+        Host "192.168.250.251"
+        Port "5555"
+        Protocol TCP
+        StoreRates false
+        CheckThresholds true
+        TTLFactor 30.0
+    </Node>
+    Tag "collectd"
+</Plugin>
+EOT
+
 # Finish modifying for the cloud by stripping the username password via -l
 echo "passwd -l vagrant" > /tmp/shutdown
 echo "rm /tmp/shutdown" >> /tmp/shutdown
